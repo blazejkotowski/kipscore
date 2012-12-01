a54fec0fe042b2bfa5229a29ea93a7b5967ddc9d
Błażej Kotowski <blaziko@gmail.com>
2012-12-01 23:13:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kipscore::Application.config.secret_token = 'b073aa586bed96bf5a03097227ecf4d6fb6ec2a5d2ca81bb8e8109bf1bfc7ac9c38a22b151639541dfcf4e96acc9b4a1d343e32f65c5fade87c0496fa05f399a'
