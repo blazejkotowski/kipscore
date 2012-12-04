ad7bf2faa32c1e7e4ca175ea04be7d9b4e4979a6
Błażej Kotowski <blaziko@gmail.com>
2012-12-04 01:35:58 +0100

    It should be project-wide

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,12 +32,4 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to root_path unless current_user? @user
     end
-    
-    def signed_in_user
-      unless signed_in?
-        store_location
-        flash[:notice] = 'Please sign in'
-        redirect_to root_url
-      end
-    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,12 +27,21 @@ module SessionsHelper
   end
   
   def return_back_or(default)
+    return_url = session[:return_to]
     session.delete(:return_to)
-    redirect_to session[:return_to] || default   
+    redirect_to return_url || default   
   end
   
   def store_location
     session[:return_to] = request.url
+  end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      flash[:notice] = 'Please sign in'
+      redirect_to root_url
+    end
   end
   
 end
