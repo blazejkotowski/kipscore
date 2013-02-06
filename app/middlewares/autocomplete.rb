f1f0d05c6e12749f3e3f9196129c6dda4665a5c9
Błażej Kotowski <blaziko@gmail.com>
2013-02-06 03:28:22 +0100

    autocomplete caching and rack app displaying

---
+++ app/middlewares/autocomplete.rb
@@ -0,0 +1,15 @@
+class Autocomplete
+  def initialize(app)
+    @app = app
+  end
+  
+  def call(env)
+    if env["PATH_INFO"] == "/players/autocomplete"
+      request = Rack::Request.new(env)
+      terms = Player.autocomplete(request.params["term"])
+      [200, {"Content-type" => "application/json"}, [terms.to_json]]
+    else
+      @app.call(env)
+    end 
+  end
+end
