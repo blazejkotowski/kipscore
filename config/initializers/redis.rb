ef8268a63efcd69c0ed0164a2fbfcd2e01c0be9e
Błażej Kotowski <blaziko@gmail.com>
2013-02-07 04:25:45 +0100

    only if env provided

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-$redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+if Rails.env.production? || Rails.env.development? || Rails.env.staging?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+end
