f532e6363fdd352c92ad0409937c3f20e816e6ce
Błażej Kotowski <blaziko@gmail.com>
2013-02-16 00:34:27 +0100

    should authenticate for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  if Rails.env.production?
+    http_basic_authenticate_with :name => 'tournaments', :password => 'tournaments'
+  end
+  
   protect_from_forgery
   include SessionsHelper
+  
 end
