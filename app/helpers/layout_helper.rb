c51e46711252a4c26323859d65a05c74d0a3ea03
Błażej Kotowski <blaziko@gmail.com>
2013-02-17 13:01:47 +0100

    Helper with layout methods

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,8 @@
+module LayoutHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Context 
+  
+  def flash_major_notice(notice)
+    flash.now[:info] = content_tag(:h3, notice)
+  end
+end
