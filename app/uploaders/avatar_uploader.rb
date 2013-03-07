4688504d1688d2e5353fa3c9bcdd1debcd02de3b
Błażej Kotowski <blaziko@gmail.com>
2013-03-07 21:19:07 +0100

    avatar placeholder

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,6 +29,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  
+  def default_url
+    "/images/avatar_placeholder.png"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
